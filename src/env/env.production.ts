bc9922a055a97924609c08b303a360fbaad01f0d
berdnaski <berdnaski1177@gmail.com>
2025-11-27 22:42:08 -0300

    refactor(env): read API_URL from runtime only

--- src/env/env.production.ts
+++ src/env/env.production.ts
@@ -1,4 +1,3 @@
 export const env = {
-  production: true,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: (window as any).__env?.API_URL
 };
--- src/env/env.ts
+++ src/env/env.ts
@@ -1,4 +1,4 @@
 export const env = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: (window as any).__env?.API_URL || (window as any).__env?.apiUrl || 'http://localhost:8080/api'
 };
